c49eb6a86cffbed612f02a7186125a4d40c4d06a
Mijael <mijael691@gmail.com>
2022-10-11 13:05:59 -0500

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
   const [paciente, setPaciente] = useState({});
-
-  useEffect(() => {
-    const obtenerLS = () => {
-      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
-      setPacientes(pacientesLS)
-    }
-    obtenerLS();
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('pacientes', JSON.stringify( pacientes ))
